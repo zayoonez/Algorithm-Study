bb04f0ce6c62cb1507e2ecc45e46663a446d175e
김자윤 <105204294+zayoonez@users.noreply.github.com>
2024-05-20 00:31:07 +0900

    [level 0] Title: 홀짝 구분하기, Time: 44.61 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.js
@@ -0,0 +1,21 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    n = Number(input[0]);
+    // if (n%2 == 0) {
+    //     console.log(n+" is even")
+    // }
+    // else {
+    //     console.log(n+ " is odd")
+    // }
+//         console.log(n%2===0? `${n} is even`:`${n} is odd`)
+    console.log(n%2 == 0 ? `${n} is even` : `${n} is odd`)
+});
